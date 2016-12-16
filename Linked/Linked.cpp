a1a60c3bb83f529bffb1772d0ca3ba8cb600840a
Donray Williams <w.donray@yahoo.com>
2016-12-15 20:15:11 -0800

    Test case complete

--- Linked/Linked.cpp
+++ Linked/Linked.cpp
@@ -3,45 +3,35 @@
 #include <Windows.h>
 #include <string>
 #include "Linked.h"
-void List(nodeType<int> * start)//, nodeType end)
-{
-	while (start != NULL)
-	{
-		std::cout << start->info;
-		start = start->link;
-	}
-}
+//void List(nodeType<int> * start)//, nodeType end)
+//{
+//	while (start != NULL)
+//	{
+//		std::cout << start->info;
+//		start = start->link;
+//	}
+//}
 int main()
 {
-	//nodeType<int> * head;
-	//nodeType<int> a, b, c;
-	//a.info = 0;
-	//b.info = 2;
-	//c.info = 4;
-	//c.link = &b;
-	//b.link = &a;
-	//a.link = NULL;
-	//head = &c;
-	//List(head);
 	linkedListType<int> * A = new linkedListType<int>();
 	linkedListType<int> * B = new linkedListType<int>();
+	linkedListType<int> * C = new linkedListType<int>();
 	A->insertLast(1);
 	A->insertLast(3);
 	A->insertLast(2);
 	A->insertFirst(54);
-	*B = *A;
-	//A->print();
-	linkedListIterator<int> tmp = linkedListIterator<int>(A->begin());
-	++tmp;
-	A->front();
 	//A->destroyList();
 	A->deleteNode(54);
 	A->print();
-	bool test = A->search(53);
-	if (test == true)
-		std::cout << "\nTrue \n\n";
-	else
-		std::cout << "\nFalse \n\n";
+	bool test, test2, test3;
+	(test = A->search(53)) ? std::cout << "\nTrue \n" : std::cout << "\nFalse \n";
+	(test2 = A->isEmptyList()) ? std::cout << "\nTrue \n" : std::cout << "\nFalse \n";
+	(B = A) ? std::cout << "\nA and B are now equal\n\n" : std::cout << "\nFalse \n";
+	std::cout << "Length: " << A->length() << "\n\n";
+	std::cout << "Front: " << A->front() << "\n\n";
+	std::cout << "Back: " << A->back() << "\n\n";
+	(B == A) ? std::cout << "They are equal \n\n" : std::cout << "";
+	(B != A) ? std::cout << "They are not equal \n \n" : std::cout << ":)\n";
 	system("pause");
 	return 0;
 }
